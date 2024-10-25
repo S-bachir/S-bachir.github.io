7d39c1f7d1956f4a0d52995d204292b035420089
bachir sabo <bachirsabo@ymail.com>
2024-10-25 02:45:19 +0200

    TS config & Magic UI config

---
+++ index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { NumberTickerProjects } from "./src/components/number-ticker-projects";
+
+ReactDOM.render(
+    <React.StrictMode>
+      <NumberTickerProjects />
+    </React.StrictMode>,
+    document.getElementById('number-ticker-projects')
+  );
